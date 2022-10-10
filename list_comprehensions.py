64ffa8a0d82a3b206bc055d657d6493262c7565f
Ben Frey <spartanhooah@gmail.com>
2022-10-10 08:43:17 -0400

    list comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,3 +2,8 @@ temps = [221, 234, 340, -9999, 230] # -9999 means "no valid data"; should be ign
 new_temps = [temp / 10 for temp in temps if temp != -9999] # list comprehension
 
 print(new_temps)
+
+# If you want to add an else, the for/in needs to be after the if/else
+new_temps = [temp / 10 if temp != -9999 else 0 for temp in temps]
+
+print(new_temps)
