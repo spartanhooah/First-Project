9ca07c9f7ac5d1c730bf1763bae8ddd68e8da15b
Ben Frey <spartanhooah@gmail.com>
2022-10-10 08:12:33 -0400

    Remove .history directory

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,2 +1,4 @@
-temps = [221, 234, 340, 230]
-new_temps = [temp / 10 for temp in temps] # list comprehension
+temps = [221, 234, 340, -9999, 230] # -9999 means "no valid data"; should be ignored
+new_temps = [temp / 10 for temp in temps if temp != -9999] # list comprehension
+
+print(new_temps)
